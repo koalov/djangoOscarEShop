bcb6723f302d4f67d957c787cf65f075b2050410
Kostyantyn Ovcharuk <koalov99@gmail.com>
2022-09-05 22:51:13 +0300

    Some small changes applied

--- frobshop/settings.py
+++ frobshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from oscar.defaults import *
 
@@ -111,7 +111,7 @@ ROOT_URLCONF = 'frobshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
